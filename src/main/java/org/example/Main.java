812b9a971c8898e20fdfc4195f1f96022f377ffa
shulga_ea <shulga23@gmail.com>
2022-12-19 14:31:35 +0300

    код для билета 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,20 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String text = "Хочу стать программистом";
+        String [] result = text.split(" ");
+        for(String st:result){
+            System.out.println(st);
+        }
+
+        String one = text.substring(0, 4);
+        String two = text.substring(5, 10);
+        String three = text.substring(11);
+        System.out.println(one);
+        System.out.println(two);
+        System.out.println(three);
+
     }
-}
+    }
