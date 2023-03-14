67221df46fc1c4a18b00eff48b139e72c6580cee
EkaterinaShulga <102359023+EkaterinaShulga@users.noreply.github.com>
2023-03-14 14:58:43 +0300

    код для билета 4 - новая ветка

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,35 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println(" new words");
 
-        String text = "Хочу стать программистом";
-        String [] result = text.split(" ");
-        for(String st:result){
-            System.out.println(st);
-        }
+        String CONTACT_TEXT_PATTERN = "([\\W+]+)(\\s)([0-9]{11})(\\s)([\\W+]+)";
+        Pattern pattern = Pattern.compile(CONTACT_TEXT_PATTERN);
+        String text = "Шарик 89061855572 Иванов Иван Иванович ";
+        Matcher matcher = pattern.matcher(text);
 
-        String one = text.substring(0, 4);
-        String two = text.substring(5, 10);
-        String three = text.substring(11);
-        System.out.println(one);
-        System.out.println(two);
-        System.out.println(three);
+       if (matcher.matches()) {
+          String name = matcher.group(1);
+           String age = matcher.group(3);
+            String color = matcher.group(5);
+
+           System.out.println(name);
+           System.out.println(age);
+           System.out.println(color);
+
+       }System.out.println(text);
+
+       List<String> words = new ArrayList<>();
+       words.add("text");
+       words.add("words");
+        System.out.println(words.toString());
 
     }
-    }
+}
+
